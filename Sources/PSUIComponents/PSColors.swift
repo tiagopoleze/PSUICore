ae0b2fe10f970d9bfad1dbc99f141ddfbfb653cc
Tiago Ferreira <tiagopoleze@icloud.com>
2023-05-15 06:58:21 +0200

    Fix hex color

--- Sources/PSUIComponents/PSColors.swift
+++ Sources/PSUIComponents/PSColors.swift
@@ -1,30 +1,30 @@
 import SwiftUI
 
-@available(iOS 13.0, macOS 10.15, *)
+@available(iOS 15.0, macOS 12.0, *)
 public extension Color {
     static func primaryPS(_ colorScheme: ColorScheme) -> Color {
         colorScheme == .light
-            ? Color("#247BA0")
-            : Color("#264653")
+            ? Color(hexValue: "#247BA0")
+            : Color(hexValue: "#264653")
     }
     static func secondaryPS(_ colorScheme: ColorScheme) -> Color {
         colorScheme == .light
-            ? Color("#FFE066")
-            : Color("#E9C46A")
+            ? Color(hexValue: "#FFE066")
+            : Color(hexValue: "#E9C46A")
     }
     static func tertiaryPS(_ colorScheme: ColorScheme) -> Color {
         colorScheme == .light
-            ? Color("#50514F")
-            : Color("#F4A261")
+            ? Color(hexValue: "#50514F")
+            : Color(hexValue: "#F4A261")
     }
     static func successPS(_ colorScheme: ColorScheme) -> Color {
         colorScheme == .light
-            ? Color("#70C1B3")
-            : Color("#2A9D8F")
+            ? Color(hexValue: "#70C1B3")
+            : Color(hexValue: "#2A9D8F")
     }
     static func dangerPS(_ colorScheme: ColorScheme) -> Color {
         colorScheme == .light
-            ? Color("#F25F5C")
-            : Color("#E76F51")
+            ? Color(hexValue: "#F25F5C")
+            : Color(hexValue: "#E76F51")
     }
 }
